15f8a06d0958e087532c61ea4733beb5579eda5f
Ricardo de Amorim <ricardo@interaxon.ca>
2023-02-17 16:50:26 -0500

    update

--- TestApp/AnagramTests/AnagramTests.swift
+++ TestApp/AnagramTests/AnagramTests.swift
@@ -1,12 +1,24 @@
 import XCTest
 
-func anagrams(targetWord: String, dictionary: [String]) -> [String] {
+func anagrams(_ targetWord: String, _ dictionary: [String]) -> [String] {
 	// complete your solution here.
 	return [];
 }
 
 class AnagramTests: XCTestCase {
 
+    let dictionary = ["tar", "rat", "married"]
+
+    func test1() {
+        XCTAssertEqual(anagrams("art", dictionary), ["tar", "rat"])
+    }
     
+    func test2() {
+        XCTAssertEqual(anagrams("admirer", dictionary), ["married"])
+    }
+    
+    func test3() {
+        XCTAssertEqual(anagrams("orange", dictionary), [])
+    }
 
 }
